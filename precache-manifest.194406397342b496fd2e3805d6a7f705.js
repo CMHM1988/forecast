016de9be781f16bbb2b53c1193df1f09501ad2c9
Manolo <cmanuelhmendez@gmail.com>
2020-06-17 01:45:41 -0500

    Updates

---
+++ precache-manifest.194406397342b496fd2e3805d6a7f705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84987843f619f83de6538aac996e1f21",
+    "url": "/forecast/index.html"
+  },
+  {
+    "revision": "23e85e8f9846acd32696",
+    "url": "/forecast/static/css/main.fca9deb4.chunk.css"
+  },
+  {
+    "revision": "22f9fdced19d088e5c5c",
+    "url": "/forecast/static/js/2.6feefc3c.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/forecast/static/js/2.6feefc3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e85e8f9846acd32696",
+    "url": "/forecast/static/js/main.f7bf3564.chunk.js"
+  },
+  {
+    "revision": "97e440b5208f5978a1b6",
+    "url": "/forecast/static/js/runtime-main.b61088ad.js"
+  },
+  {
+    "revision": "a7b0cdeb55e5ca07219c2aa22010df31",
+    "url": "/forecast/static/media/not-found.a7b0cdeb.jpg"
+  }
+]);
