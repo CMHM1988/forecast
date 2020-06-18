7ea46a4e14fc0fb87214fb21fa02e0b38ab35ffa
Manolo <cmanuelhmendez@gmail.com>
2020-06-17 19:48:12 -0500

    Estilando datos del clima

--- src/components/Searcher/Weather.js
+++ src/components/Searcher/Weather.js
@@ -8,12 +8,18 @@ function Weather({values}) {
    const { name, main } = values;
    // Retornando componente.
    return (
-      <aside className="weather-container grid">
-         <label htmlFor="" className="font-bold result-name">{name}</label>
-         <p>
-            <label htmlFor="" className="font-bold">Temperature: </label>
-            <label htmlFor="">{parseFloat(main.temp - 273.15, 10).toFixed(2)} °C</label>
+      <aside className="weather-container grid font14">
+         <label htmlFor="" className="font-bold result-name">The weather in {name} City is</label>
+         <p className="grid-justify-self-center">
+            <label className="font50 blue" htmlFor="">{parseFloat(main.temp - 273.15, 10).toFixed(0)} &#8451;</label>
          </p>
+         <p className="grid-justify-self-center">
+            <label htmlFor="" className="font-bold">Max: </label>
+            <label className="mright25" htmlFor="">{parseFloat(main.temp_max - 273.15, 10).toFixed(2)} &#8451;</label>
+            <label htmlFor="" className="font-bold">Min: </label>
+            <label htmlFor="">{parseFloat(main.temp_min - 273.15, 10).toFixed(2)} &#8451;</label>
+         </p>
+         <p className="font-bold red">Additional Information</p>
          <p>
             <label htmlFor="" className="font-bold">Pressure: </label>
             <label htmlFor="">{main.pressure} pci</label>
@@ -22,13 +28,9 @@ function Weather({values}) {
             <label htmlFor="" className="font-bold">Humidity: </label>
             <label htmlFor="">{main.humidity} %</label>
          </p>
-         <p>
-            <label htmlFor="" className="font-bold">Max temperature: </label>
-            <label htmlFor="">{parseFloat(main.temp_max - 273.15, 10).toFixed(2)} °C</label>
-         </p>
-         <p>
-            <label htmlFor="" className="font-bold">Min temperature: </label>
-            <label htmlFor="">{parseFloat(main.temp_min - 273.15, 10).toFixed(2)} °C</label>
+         <p className="font10 grid-align-self-end">
+            <label htmlFor="" className="font-bold">Weather data from: </label>
+            <label htmlFor="">https://openweathermap.org/</label>
          </p>
       </aside>
    );
